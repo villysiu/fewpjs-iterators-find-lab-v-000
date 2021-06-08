5fc1b59ff267fe0b460af488f98a6587d95f3930
villysiu <villysiu@gmail.com>
2021-06-08 11:32:23 -0700

    commit tree 13d1f563aa1bd0bb69b32a0abb46c8a07ea78954

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = function(arr) {
+  let winningGame = arr.find(game => game.result === "W");
+  if (winningGame) {
+    return winningGame.year;
+}
+  else {
+    return undefined;
+  }
+}
